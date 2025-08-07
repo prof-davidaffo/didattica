0ac572b0dc580cb6063674bde7ebfa93f62f50bf
davidaffo <davidedaffo@gmail.com>
2025-08-07 18:35:49 +0200

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "/didattica/",
     ignorePatterns: ["private", "templates", ".obsidian","**/*[Vv]erific*.*", "**/*[Ss]oluz*.*"],
     defaultDateType: "modified",
     theme: {
