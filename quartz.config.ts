aad9c5aa2283c2cfec6550c934d20ff1cc7fcc0c
davidaffo <davidedaffo@gmail.com>
2025-08-07 18:29:27 +0200

    first deploy

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian","**/*[Vv]erific*.*", "**/*[Ss]oluz*.*"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
