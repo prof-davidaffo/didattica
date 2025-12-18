7847208bb62ec5fa5b4fe52084302823eb5e3ebd
davidaffo <davidedaffo@gmail.com>
2025-12-18 12:37:47 +0100

    Quartz sync: Dec 18, 2025, 12:37 PM

---
+++ quartz/static/ads-refresh.js
@@ -0,0 +1,8 @@
+document.addEventListener("spa:navigation", () => {
+  try {
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  } catch (e) {
+    console.warn("Ads refresh failed", e);
+  }
+});
+
